4ab2201ef0968ab7e7e9d00f7a4901b7112783b6
Navjot Singh <weavebytes@gmail.com>
2017-04-08 16:07:10 +0530

    sql for admin table

---
+++ sql/db.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int(64) NOT NULL,
+  `username` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `email` varchar(128) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+
+
