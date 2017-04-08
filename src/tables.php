f79c577d34f265af154546e347de2b0817f2768f
Navjot Singh <weavebytes@gmail.com>
2017-04-08 17:00:00 +0530

    update title

--- src/tables.php
+++ src/tables.php
@@ -1,6 +1,6 @@
 <html lang="en">
 <head>
-  <title>License Plates</title>
+  <title>Trucking Admin</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
