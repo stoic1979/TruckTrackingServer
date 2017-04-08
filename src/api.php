c9d8e0ef08c004f6ddc8d46836b52e08a6a44bc0
Navjot Singh <weavebytes@gmail.com>
2017-04-08 23:46:57 +0530

    updated query

--- src/api.php
+++ src/api.php
@@ -23,7 +23,7 @@ function userExists($username){
 //////////////////////////////////////////////////////
 
 function didExists($did){
-    $qry = "SELECT did FROM driver WHERE id=$did";	
+    $qry = "SELECT id FROM driver WHERE id=$did";	
     $result = mysql_query($qry);
     return mysql_num_rows($result) >= 1;
 }
